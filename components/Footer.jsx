3d8aae6168eb52ea1f693f9f4c2e804867323d40
Ramprasath L S <lsramprasath@gmail.com>
2025-09-21 18:47:57 -0500

    reset: remove all files

--- components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-  return (
-    <footer className="container py-10 text-sm text-gray-400">
-      © {new Date().getFullYear()} Your Name. All rights reserved.
-    </footer>
-  );
-}
